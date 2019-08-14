ce7a5bcb8455bc16023bbad18f5c0d545f0589c6
rohit236c <rohit236c@gmail.com>
2019-08-14 19:29:18 +0530

    spoj problm
    Signed-off-by: rohit236c <rohit236c@gmail.com>

---
+++ slidingWindow.cpp
@@ -0,0 +1,41 @@
+/*
+	author of this code is rohit sharma
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int arr[100];
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+	int k;
+	cin >> k;
+	deque<int>d(k);
+	int i;
+	for(i = 0; i < k; i++) {
+		while(!d.empty() && arr[i]>arr[d.front()]){
+			d.pop_back();
+		}
+		d.push_back(i);
+	}
+	for(;i<n;i++){
+		cout<<arr[d.front()]<<" ";
+
+		while(!d.empty() && i-k >= d.front()){
+			d.pop_front();
+		}
+		while(!d.empty() && arr[i]>arr[d.front()]){
+			d.pop_back();
+		}
+		d.push_back(i);
+	}
+
+	cout<<arr[d.front()];
+
+	return 0;
+}
