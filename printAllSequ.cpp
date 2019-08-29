632689fdb83ce5046195247e757932c7037ee8fc
rohit236c <rohit236c@gmail.com>
2019-08-29 21:52:44 +0530

    gfg-method 2 added
    Signed-off-by: rohit236c <rohit236c@gmail.com>

---
+++ printAllSequ.cpp
@@ -0,0 +1,12 @@
+/*
+	author of this code is rohit sharma
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+
+
+	return 0;
+}
--- printAllSequence.cpp
+++ printAllSequence.cpp
@@ -4,13 +4,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printAllSequencesUtil(std::map<int, std::vector<int>>&map,int*arr,int n,int index){
-	if(index == n){
+// method 1
+void printAllSequencesUtil(std::map<int, std::vector<int>>&map, int*arr, int n, int index) {
+	//base case
+	if (index == n) {
 		return;
 	}
 	vector<int> v;
-	
-	if(map.count(arr[index]) == 0){
+
+	if (map.count(arr[index]) == 0) {
 		map[arr[index]];
 	}
 	for (int i = index + 1; i < n; ++i)
@@ -18,35 +20,68 @@ void printAllSequencesUtil(std::map<int, std::vector<int>>&map,int*arr,int n,int
 		v.push_back(arr[i]);
 	}
 	map[arr[index]] = v;
-
-	printAllSequencesUtil(map,arr,n,index+1);
-
+	printAllSequencesUtil(map, arr, n, index + 1);
 }
 
-void printAllSequences(int * arr,int n,int r){
+void printAllSequences(int * arr, int n, int r) {
 	std::map<int, vector<int> > mymap;
 	int index = 0;
-	printAllSequencesUtil(mymap,arr,n,index);
 
-	for(auto element: mymap){
+	printAllSequencesUtil(mymap, arr, n, index);
+
+	for (auto element : mymap) {
 		for (int i = 0; i < element.second.size(); ++i)
 		{
-			cout<<element.first<<"->"<<element.second[i]<<" ";
+			cout << element.first << "->" << element.second[i] << " ";
 		}
-		cout<<endl;
+		cout << endl;
 	}
 }
 
-int main(){
-	int n,r;
-	cin>>n>>r;
+//method 2
+
+void printCombinationUtil(int*arr, int*data, int start, int end,int index ,int r) {
+
+	//base case
+	if (index == r) {
+		//print the data array
+		for (int i = 0; i < r; i++) {
+			cout << data[i];
+		}
+		cout << endl;
+		return;
+	}
+
+	// replace index with all possible  
+    // elements. The condition "end-i+1 >= r-index" 
+    // makes sure that including one element  
+    // at index will make a combination with  
+    // remaining elements at remaining positions  
+    
+	for(int i = start;i<=end && end-i+1 >= r-index;i++){
+		data[index] = arr[i];
+		printCombinationUtil(arr,data,i+1,end,index+1,r);
+	}
+
+}
+
+void printCombination(int*arr, int n, int r) {
+	int data[r];
+
+	printCombinationUtil(arr, data, 0, n - 1, 0, r);
+
+}
+
+int main() {
+	int n, r;
+	cin >> n >> r;
 	int arr[100] = {0};
 	for (int i = 0; i < n; ++i)
 	{
-		cin>>arr[i];
+		cin >> arr[i];
 	}
-	printAllSequences(arr,n,r);
-	
+	printAllSequences(arr, n, r);
+
 
 	return 0;
 }
